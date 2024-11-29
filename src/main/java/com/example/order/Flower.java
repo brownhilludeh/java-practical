d6e9fde334b72e00cdeb1433c0e2edcc11c8fe2a
brownhilludeh <brownhilludeh@gmail.com>
2024-11-29 15:00:24 -0400

    float_meth

--- src/main/java/com/example/order/Flower.java
+++ src/main/java/com/example/order/Flower.java
@@ -1,11 +1,16 @@
 package com.example.order;
 
+import com.example.util.Utilities;
+
 public class Flower {
 
+    protected int number;
+    private float floatNumber;
 
     public void getInfo(){
         System.out.println("who is me?");
-        
+        number = Utilities.getInt("Enter any number");
+        floatNumber = Utilities.getFloat("Enter a deciamla place number.");
     }
 
     public void name() {
--- src/main/java/com/example/util/Utilities.java
+++ src/main/java/com/example/util/Utilities.java
@@ -1,6 +1,12 @@
 package com.example.util;
 
-/*
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+import javax.print.DocFlavor.READER;
+import javax.sound.sampled.FloatControl;
+
+/**
  * Has some useful methods to be used in my project
  * 
  * @author Brownhill U
@@ -8,4 +14,59 @@ package com.example.util;
  */
 public class Utilities {
 
+    private static Scanner scan = new Scanner(System.in);
+
+    /**
+     * Getting string input
+     * 
+     * @param String prompt
+     * @return string
+     * @author Brownhill U
+     * @since Nov 30
+     */
+    public static String getString(String prompt) {
+        System.out.println(prompt + ": ");
+        String result = scan.nextLine();
+        return result;
+    }
+
+    /**
+     * 
+     * @param Integer prompt
+     * @return result 
+     * @author Brownhill U
+     * @since Nov 30
+     */
+    public static int getInt(String prompt) {
+        // String inputString = getString(prompt);
+        // int result = Integer.parseInt(inputString);
+        // return result;
+        int result;
+        while (true) {
+            try {
+                System.out.println(prompt);
+                String inputString = scan.nextLine();
+                result = Integer.parseInt(inputString);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid integer number.");
+            }
+        }
+        return result;
+    }
+
+    public static float getFloat(String prompt) {
+        float result;
+        while (true) {
+            try {
+                System.out.println(prompt);
+                String inputString = scan.nextLine();
+                result = Float.parseFloat(inputString);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid float number.");
+            }
+        }
+        return result;
+    }
 }
